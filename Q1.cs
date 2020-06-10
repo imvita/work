2db5bf1256ffe6226a63412af557c39376a940d7
imvita <66725264+imvita@users.noreply.github.com>
2020-06-10 23:33:09 +0800

    Add files via upload

---
+++ Q1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string putin;
+            string putout;
+            int leng;
+            Console.Write("輸入字串");
+            putin = Console.ReadLine();
+            leng = putin.Length;
+            putout = null;
+            int i;
+            for (i=0; i<leng; i++)
+            {
+                putout += putin[leng-i-1];
+            }
+            Console.WriteLine("{0}",putout);
+            Console.Read();
+        }
+    }
+}
---
+++ Q2.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("輸入數字");
+            int input = Console.ReadLine();
+            int i;
+            Vector a;
+            int i3 = Math.Floor(input / 3);
+            int i5 = Math.Floor(input/5);
+            for (i=0; i<input; i++)
+            {
+                a[i] = i + 1;
+                if (a[i] % 3 == 0 || a[i] % 5 == 0)
+                {
+                    a[i] = 0;
+                }
+                else if (i % 15 == 0)
+                {
+                    a[i] = i + 1;
+                }
+            }
+            a.Replace("0","");
+            Console.WriteLine(a);
+            Console.Read();
+        }
+    }
+}
